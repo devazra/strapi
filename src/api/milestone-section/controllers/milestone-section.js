6c8117fa6b22f95d49d82b1415d5bbb9aef5dcfd
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-12-29 13:49:59 +0500

    update

---
+++ src/api/milestone-section/controllers/milestone-section.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * milestone-section controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::milestone-section.milestone-section');
